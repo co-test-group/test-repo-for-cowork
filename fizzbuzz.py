0003a680f51cb37b486bdbcc5dd38c69f77e823a
soft3014 <softsoft3014@gmail.com>
2023-11-19 02:56:01 +0900

    fefactor: add comments in fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 for i in range(1, 16+1):
-    if i % 15==0:
+    if i % 15==0:   # 15의 배수일 경우 fizzbuzz
         print('fizzbuzz')
-    elif i%3==0:
+    elif i%3==0:    # 3의 배수일 경우 fizz
         print('fizz')
-    elif i%5==0:
+    elif i%5==0:    # 5의 배수일 경우 buzz
         print('buzz')    
     else:
         print(i)
